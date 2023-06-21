c06560652f61b397da098be0d2128d0477a807e7
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-06-21 12:19:20 +1200

    wip

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -2,7 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
 	const { locals } = event;
-	locals.LIMIT = 2;
+	locals.LIMIT = 10;
 	const response = await resolve(event);
 	return response;
 }) satisfies Handle;
