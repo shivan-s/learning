76f470d3d8d04283311b5f1741cc024e69e32747
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-06-25 09:38:36 +1200

    wip

--- client/src/lib/db/db.ts
+++ client/src/lib/db/db.ts
@@ -26,8 +26,9 @@ export interface Database {
 }
 
 export default function connection(db: D1Database) {
-	/* const dialect = db ? new D1Dialect({ database: db }) : new SqliteDialect({ database: dummyDb() }); */
+	console.log(SqliteDialect);
 	const dialect = db && new D1Dialect({ database: db });
+	/* const dialect = db ? new D1Dialect({ database: db }) : new SqliteDialect({ database: dummyDb() }); */
 	if (!db) {
 		console.warn('Connecting to Dummy database');
 	}
